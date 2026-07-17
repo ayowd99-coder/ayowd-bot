437919bdf85a19ec8679679f1f9768d4176c9f41
ayowd99-coder <ayowd99@gmail.com>
2026-07-17 20:46:27 +0700

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -45,7 +45,7 @@ bot.onText(/\/start/, async (msg) => {
   const firstName = msg.from?.first_name || "Kak";
   chatHistory.set(chatId, []);
   try {
-    await bot.sendPhoto(chatId, "https://i.postimg.cc/pdqCNbzG/baru.png", {
+    await bot.sendPhoto(chatId, "https://i.postimg.cc/HsR3ZV4Q/brand.png", {
       caption: `Halo Kak <b>${firstName.toUpperCase()}</b>, selamat datang di layanan Customer Service AYOWD.\n\nAda yang bisa kami bantu hari ini?\n\n👇 <i>Akses Cepat:</i>`,
       parse_mode: "HTML",
       reply_markup: {
